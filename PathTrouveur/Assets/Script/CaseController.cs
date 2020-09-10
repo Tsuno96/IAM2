2c4cf150520865f4290cfe1bfa11faea7a917d46
mlivebardo <tsunoll96@gmail.com>
2020-09-10 17:22:18 +0200

    On instancie
    Terrain + case
    *Case ->  position, voisins, poids

---
+++ PathTrouveur/Assets/Script/CaseController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseController : MonoBehaviour
+{
+    public int i,j;
+    public List<GameObject> arrGO_neighbour;
+    public float poids;
+
+    
+    void Awake()
+    {
+        arrGO_neighbour = new List<GameObject>();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if(poids >1)
+        {
+            this.gameObject.GetComponent<Renderer>().material = MGR.Instance.mat_Obstacle;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setPos(int _i, int _j)
+    {
+        i = _i;
+        j = _j;
+    }
+
+    public void setPoids(float f)
+    {
+        poids = f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!arrGO_neighbour.Contains(other.gameObject))
+        {
+            arrGO_neighbour.Add(other.gameObject);
+        }
+    }
+}
---
+++ PathTrouveur/Assets/Script/MGR.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MGR : MonoBehaviour
+{
+    private static MGR p_instance = null;
+    public static MGR Instance { get { return p_instance; } }
+
+    public Material mat_Obstacle;
+
+    public GameObject GO_Plane;
+    public GameObject GO_Case;
+
+    public GameObject[,] arrGO_Cases;
+
+    int[,] layout;
+
+    public Vector2 vec2_Depart;
+    public Vector2 vec2_Arrive;
+
+
+
+
+    //public int length;
+    //public int width;
+    void Awake()
+    {
+        if (p_instance == null)
+            //if not, set instance to this
+            p_instance = this;
+        //If instance already exists and it's not this:
+        else if (p_instance != this)
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        arrGO_Cases = new GameObject[10, 10];
+        layout = new int[10, 10]
+        {
+        {1,0,1,1,1,1,1,1,1,1},
+        {1,0,1,1,1,1,1,1,1,1},
+        {1,0,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1}
+        };
+
+
+
+        Instantiate(GO_Plane.transform, new Vector3(5, 0, 5), Quaternion.identity);
+        for(int i = 0; i< 10;i++)
+            for(int j = 0; j <10; j++)
+            {
+                arrGO_Cases[i,j] = (GameObject)Instantiate(GO_Case, new Vector3(i+0.5f, 0.5f, j+0.5f), Quaternion.identity);
+                arrGO_Cases[i, j].GetComponent<CaseController>().setPos(i, j);
+                arrGO_Cases[i, j].gameObject.name = "Case_" + i + "_" + j; 
+                if(layout[i,j] == 1)
+                {
+                    arrGO_Cases[i, j].GetComponent<CaseController>().setPoids(1.0f);
+                }
+                else
+                {
+                    arrGO_Cases[i, j].GetComponent<CaseController>().setPoids(Mathf.Infinity);
+                }
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
