a26f6144ffeab6f187c8a6ae8d60335e2213cc6d
mlivebardo <tsunoll96@gmail.com>
2020-10-22 12:06:33 +0200

    Apprentissage

--- Fanned/UnderstandFANN/UnderstandFANN/Program.cs
+++ Fanned/UnderstandFANN/UnderstandFANN/Program.cs
@@ -15,45 +15,30 @@ namespace UnderstandFANN
         static void Main(string[] args)
         {
 
-            string[] lang = new string[3] { "Polonais", "English", "Francais" };
-            string text = System.IO.File.ReadAllText(@"D:\M2\IAM2\Fanned\UnderstandFANN\UnderstandFANN\text"+lang[2]+".txt").ToString();
-            byte[] tempBytes;
-            tempBytes = System.Text.Encoding.GetEncoding("ISO-8859-8").GetBytes(text);
-            string asciiStr = System.Text.Encoding.UTF8.GetString(tempBytes);
-            string[] lettres = new string[26];
-
-            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower().ToCharArray();
-            string textisLetter="";
-            for (int i = 0; i < asciiStr.Length; i++)
-            {
-                if (Char.IsLetter(asciiStr[i]))
-                {
-                    textisLetter += asciiStr[i];
-                }
-            }
-
-            Dictionary<char, int> dict = textisLetter.ToLower().GroupBy(c => c)
-                             .ToDictionary(gr => gr.Key, gr => gr.Count());
-
-            foreach(var item in dict.Keys)
-            {
-                Console.WriteLine(item + " : " + dict[item]);
-            }
-
+        
             double[][] inputs =
             {
-                new double[]{0.103, 0.016, 0.054, 0.060, 0.113, 0.010, 0.010, 0.048, 0.056, 0.003, 0.010, 0.035,
-                    0.014, 0.065, 0.075, 0.013, 0.000, 0.051, 0.083, 0.111, 0.030, 0.008, 0.019, 0.000, 0.016, 0.000 },
-                new double[]{0.076, 0.010, 0.022, 0.039, 0.151, 0.013, 0.009, 0.009, 0.081, 0.001, 0.000, 0.058, 
-                    0.024, 0.074, 0.061, 0.030, 0.011, 0.069, 0.100, 0.074, 0.059, 0.015, 0.000, 0.009, 0.003, 0.003, },
-                new double[]{0.088, 0.016, 0.030, 0.034, 0.089, 0.004, 0.011, 0.023, 0.071, 0.032, 0.030, 0.025,
-                    0.047, 0.058, 0.093, 0.040, 0.000, 0.062, 0.044, 0.035, 0.039, 0.002, 0.044, 0.000, 0.037, 0.046 }
+                GetFrequencies("appEnglish1"),
+                GetFrequencies("appEnglish2"),
+                GetFrequencies("appEnglish3"),
+                GetFrequencies("appFrancais1"),
+                GetFrequencies("appFrancais2"),
+                GetFrequencies("appFrancais3"),
+                GetFrequencies("appPolonais1"),
+                GetFrequencies("appPolonais2"),
+                GetFrequencies("appPolonais3")
             };
 
             double[][] outputs =
             {
                 new double[]{1,0,0},
+                new double[]{1,0,0},
+                new double[]{1,0,0},
                 new double[]{0,1,0},
+                new double[]{0,1,0},
+                new double[]{0,1,0},
+                new double[]{0,0,1},
+                new double[]{0,0,1},
                 new double[]{0,0,1}
             };
 
@@ -70,32 +55,11 @@ namespace UnderstandFANN
             network.TrainOnData(data, 3000, 100, 0.001f);
 
             Console.WriteLine("Final Error :" + network.MSE);
-            
-            int sum = dict.Sum(x => x.Value);
-            double[] test = new double[26];
-            for(int i = 0; i<26;i++)
-            {
-                int value = 0;
-                if(dict.TryGetValue(alpha[i], out value))
-                {
-                    //Console.WriteLine((double)value / (double)sum +" "+i);
-                    test[i] = (double)value / (double)sum;
-                    test[i] = Math.Truncate(test[i] * 1000) / 1000;
-                }
-                else
-                {
-                    test[i] = 0.0;
-                }
-                Console.WriteLine(test[i]);
-            }
-            double res = 0;
-            foreach(double d in test)
-            {
-                res += d;
-            }
-            Console.WriteLine(res);
 
 
+
+             
+            double[] test = GetFrequencies("textEnglish");
             double[] result = network.Run(test);
 
 
@@ -104,6 +68,60 @@ namespace UnderstandFANN
             Console.WriteLine("Polonais : {0}", result[2]);
 
         }
+
+
+        static double[] GetFrequencies(string txtFilename)
+        {
+            string text = System.IO.File.ReadAllText(@"..\texts\" + txtFilename + ".txt").ToString();
+            byte[] tempBytes;
+            tempBytes = System.Text.Encoding.GetEncoding("ISO-8859-8").GetBytes(text);
+            string asciiStr = System.Text.Encoding.UTF8.GetString(tempBytes);
+            string[] lettres = new string[26];
+
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower().ToCharArray();
+            string textisLetter = "";
+            for (int i = 0; i < asciiStr.Length; i++)
+            {
+                if (Char.IsLetter(asciiStr[i]))
+                {
+                    textisLetter += asciiStr[i];
+                }
+            }
+
+            Dictionary<char, int> dict = textisLetter.ToLower().GroupBy(c => c)
+                             .ToDictionary(gr => gr.Key, gr => gr.Count());
+
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine(item + " : " + dict[item]);
+            }
+            int sum = dict.Sum(x => x.Value);
+            double[] freq = new double[26];
+            for (int i = 0; i < 26; i++)
+            {
+                int value = 0;
+                if (dict.TryGetValue(alpha[i], out value))
+                {
+                    //Console.WriteLine((double)value / (double)sum +" "+i);
+                    freq[i] = (double)value / (double)sum;
+                    freq[i] = Math.Truncate(freq[i] * 1000) / 1000;
+                }
+                else
+                {
+                    freq[i] = 0.0;
+                }
+                Console.WriteLine(freq[i]);
+            }
+            double res = 0;
+            foreach (double d in freq)
+            {
+                res += d;
+            }
+            Console.WriteLine(res);
+            return freq;
+        }
+
+
 
     }
 }
